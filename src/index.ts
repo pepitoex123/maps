0fac1b25369c19b15929bc451e016ba371ebf000
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-07 17:18:23 -0300

    added markers

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,9 @@ import User from "./User";
 import { Company } from "./Company";
 import { CustomMap } from "./CustomMap";
 
-const customMap = new CustomMap("map");
+const user = new User()
+const company = new Company();
 
+const customMap = new CustomMap("map");
+customMap.addUserMarker(user)
+customMap.addCompanyMarker(company)
